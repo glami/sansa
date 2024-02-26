e29024785217d8429ca911977eec4c25eb567ab9
matospiso <masp430@gmail.com>
2024-02-26 11:36:54 +0100

    src: add core

---
+++ src/sansa/core/__init__.py
@@ -0,0 +1,17 @@
+from .factorizers import (
+    FactorizationMethod,
+    GramianFactorizer,
+    GramianFactorizerConfig,
+    CHOLMODGramianFactorizer,
+    CHOLMODGramianFactorizerConfig,
+    ICFGramianFactorizer,
+    ICFGramianFactorizerConfig,
+)
+
+from .inverters import (
+    ApproximateInverseMethod,
+    UnitLowerTriangleInverter,
+    UnitLowerTriangleInverterConfig,
+    UMRUnitLowerTriangleInverter,
+    UMRUnitLowerTriangleInverterConfig,
+)
---
+++ src/sansa/core/_ops/__init__.py
@@ -0,0 +1,2 @@
+from ._factor_ops import icf
+from ._inverse_ops import s1, umr, get_residual_matrix
