85ba2170c19f5414bdd72032f9d77ecc3fe44c30
matospiso <masp430@gmail.com>
2024-03-03 18:57:39 +0100

    [src/sansa/__init__.py] expose factorizer configs and inverter config

--- src/sansa/__init__.py
+++ src/sansa/__init__.py
@@ -4,4 +4,5 @@ __version__ = importlib.metadata.version("sansa")
 
 import sansa.core
 import sansa.utils
+from sansa.core import CHOLMODGramianFactorizerConfig, ICFGramianFactorizerConfig, UMRUnitLowerTriangleInverterConfig
 from sansa.model import SANSA, SANSAConfig
